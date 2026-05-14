ec334fbe79ad0fd0b07f5a11c342a59249d6ab50
jahelaariza06-ctrl <jahelaariza06@gmail.com>
2026-05-14 17:07:00 -0500

    feat: agrego archivo de seguimiento de tareas del equipo

---
+++ tareas.py
@@ -0,0 +1,15 @@
+tareas_pendientes = [
+'crear modulo de prestamo de libros',
+'diseñar pantalla de inicio',
+]
+tareas_completadas = [
+' crear de base de datos',
+]
+print('=== TAREAS PENDIENTES ===')
+for t in tareas_pendientes:
+    print(f' {t}') 
+print('=== TAREAS COMPLETADAS ===')
+for t in tareas_completadas:
+    print(f' {t}') 
+
+tareas_completadas
